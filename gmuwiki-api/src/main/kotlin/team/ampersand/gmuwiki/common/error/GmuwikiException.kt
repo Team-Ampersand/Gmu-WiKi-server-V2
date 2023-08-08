35f79f7eb5e2a8e4a1850edce836aa89a65a5036
aesperer <piyrw9754@gmail.com>
2023-08-08 23:35:11 +0900

    create :: exception filter

--- gmuwiki-api/src/main/kotlin/team/ampersand/gmuwiki/common/error/GmuwikiException.kt
+++ gmuwiki-api/src/main/kotlin/team/ampersand/gmuwiki/common/error/GmuwikiException.kt
@@ -1,7 +1,7 @@
 package team.ampersand.gmuwiki.common.error
 
 abstract class GmuwikiException(
-    private val errorProperty: ErrorProperty
+    val errorProperty: ErrorProperty
 ) : RuntimeException() {
 
     override fun fillInStackTrace() = this
---
+++ gmuwiki-infra/src/main/kotlin/team/ampersand/gmuwiki/global/filter/ExceptionFilter.kt
@@ -0,0 +1,47 @@
+package team.ampersand.gmuwiki.global.filter
+
+import com.fasterxml.jackson.databind.ObjectMapper
+import io.sentry.Sentry
+import jakarta.servlet.FilterChain
+import jakarta.servlet.http.HttpServletRequest
+import jakarta.servlet.http.HttpServletResponse
+import org.springframework.http.MediaType
+import org.springframework.web.filter.OncePerRequestFilter
+import team.ampersand.gmuwiki.common.error.ErrorProperty
+import team.ampersand.gmuwiki.common.error.GmuwikiException
+import team.ampersand.gmuwiki.global.error.ErrorResponse
+import team.ampersand.gmuwiki.global.exception.InternalServerException
+import java.nio.charset.StandardCharsets
+
+class ExceptionFilter(
+    private val objectMapper: ObjectMapper
+) : OncePerRequestFilter() {
+
+    override fun doFilterInternal(
+        request: HttpServletRequest,
+        response: HttpServletResponse,
+        filterChain: FilterChain,
+    ) {
+        try{
+            filterChain.doFilter(request, response)
+        } catch(e : Exception){
+            when(e.cause){
+                is GmuwikiException -> {
+                    errorToJson((e.cause as GmuwikiException).errorProperty, response)
+                }
+
+                is Exception -> {
+                    errorToJson(InternalServerException.errorProperty, response)
+                }
+            }
+            Sentry.captureException(e)
+        }
+    }
+
+    private fun errorToJson(errorProperty: ErrorProperty,response: HttpServletResponse) {
+        response.status = errorProperty.status()
+        response.characterEncoding = StandardCharsets.UTF_8.name()
+        response.contentType = MediaType.APPLICATION_JSON_VALUE
+        response.writer.write(objectMapper.writeValueAsString(ErrorResponse.of(errorProperty)))
+    }
+}
