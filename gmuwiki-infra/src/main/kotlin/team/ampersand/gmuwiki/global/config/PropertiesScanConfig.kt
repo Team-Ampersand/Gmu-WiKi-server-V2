57ed67133fa7aadbc8f58dcf38208ceb0c1a1dda
aesperer <piyrw9754@gmail.com>
2023-08-10 22:41:40 +0900

    create :: configuration 클래스

---
+++ gmuwiki-infra/src/main/kotlin/team/ampersand/gmuwiki/global/config/PropertiesScanConfig.kt
@@ -0,0 +1,8 @@
+package team.ampersand.gmuwiki.global.config
+
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan
+import org.springframework.context.annotation.Configuration
+
+@ConfigurationPropertiesScan
+@Configuration
+class PropertiesScanConfig
